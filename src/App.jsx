f56a4044debefb329346c2f527cb8f19d78dd336
i-m-prakash <impcakash@gmail.com>
2025-01-30 12:27:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-// import "./App.css";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-slate-200 text-center p-4">
+      <h1 className="text-3xl font-bold underline bg-slate-200 text-red-700 text-center p-4">
         Hello world!
       </h1>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem]overflow-hidden"></div>
+
+      <button className="mt-10" href="#login">
+        Something
+      </button>
+
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
