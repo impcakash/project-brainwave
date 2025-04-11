362a26506c0b9de3884738493efc2b3a085e153b
impcakash <impcakash@gmail.com>
2025-04-11 15:22:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-slate-200 text-red-700 text-center p-4">
-        Hello world!
-      </h1>
-
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
-
-      <button className="mt-10" href="#login">
-        Something
-      </button>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
 
       <ButtonGradient />
     </>
